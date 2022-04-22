af8d0dead8e5f360d3e673b2b1ef96c73aa64a99
70br <elias70br@hotmail.com>
2022-04-22 10:41:49 -0300

    inclusao projeto erro

---
+++ playground.js
@@ -0,0 +1,32 @@
+function validaArrays(arr, num) {
+    try {
+     if (!arr && !num) throw new ReferenceError('Envie os parâmetros!');
+ 
+     if (typeof arr !== 'object')
+             throw new TypeError('Envie um elemento do tipo Array!');
+ 
+         if (typeof num !== 'number')
+             throw new TypeError('Envie um elemento do tipo Number!');
+ 
+         if (arr.length !== num) throw new RangeError('Tamanho do array inválido!');
+ 
+         return arr;
+    } catch(e) {
+     if (e instanceof ReferenceError) {
+         console.log("Este erro é um RefenceError!")
+         console.log(e.message)
+         
+     } else if (e instanceof TypeError) {
+         console.log("Este erro é um TypeError");
+         console.log(e.message);
+     } else if(e instanceof RangeError){
+         console.log("Este erro é um RangeError");
+         console.log(e.message);
+     } else {
+         console.log("Tipo de erro não esperado :" + e);
+     }
+        
+    }
+ }
+ 
+ console.log(validaArrays([], 'a'));
